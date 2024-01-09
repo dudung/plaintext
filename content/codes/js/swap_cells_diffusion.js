f468cc40cbb1cb161d86be707b559f9e5e447979
Sparisoma Viridi <dudung@gmail.com>
2024-01-09 19:38:17 +0700

    update

--- content/codes/js/swap_cells_diffusion.js
+++ content/codes/js/swap_cells_diffusion.js
@@ -10,3 +10,83 @@
 */
 
 console.log("swap cells diffusion");
+
+let m = [
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+]
+
+function mat2str(m) {
+  let s = "";
+  for(l of m) {
+    let si = l.join('');
+    s += si + "\n";
+  }
+  s = s.slice(0, -1);
+  return s;
+}
+
+let ta = document.createElement("textarea");
+with(ta.style) {
+  width = "360px";
+  height = "330px";
+  overflowY = "scroll";
+  letterSpacing = "0.7em";
+  float = "left";
+}
+
+let can = document.createElement("canvas");
+with(can.style) {
+  border = "1px solid #888";
+  height = "200px";
+  width = "200px";
+  float = "left";
+}
+
+let div = document.createElement("div");
+with(div.style) {
+  border = "0px solid blue";
+  width = "60px";
+  height = "200px";
+  float = "left";
+}
+
+let btn1 = document.createElement("button");
+btn1.style.width = "60px";
+btn1.innerHTML = "Clear";
+btn1.addEventListener("click", () => { ta.value = ""});
+
+let btn2 = document.createElement("button");
+btn2.style.width = "60px";
+btn2.innerHTML = "Load";
+btn2.addEventListener("click", () => { ta.value = mat2str(m)});
+
+let btn3 = document.createElement("button");
+btn3.style.width = "60px";
+btn3.innerHTML = "Step";
+
+document.body.append(ta);
+document.body.append(div);
+  div.append(btn1);
+  div.append(btn2);
+  div.append(btn3);
+document.body.append(can);
+
