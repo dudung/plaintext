9233bfc137e252b14648397d0cd41d98be88e8ce
Sparisoma Viridi <dudung@gmail.com>
2024-01-09 18:03:41 +0700

    new code

---
+++ content/codes/js/swap_cells_diffusion.js
@@ -0,0 +1,12 @@
+/*
+  swap_cells_diffusion.js
+  Perform diffusion by swapping pair of cells
+  
+  Sparisoma Viridi | https://github.com/dudung
+  
+  20240109 Create this example.
+  
+  Execute: <script src="swap_cells_diffusion.js"></script>
+*/
+
+console.log("swap cells diffusion");
